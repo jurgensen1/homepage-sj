583807065321ad9a3cc7f02bb6610523d97d2d30
Stephen Jurgensen <sdjurgy@gmail.com>
2023-01-24 20:05:47 -0800

    update for push

--- js/sliderv2.js
+++ js/sliderv2.js
@@ -1,13 +1,19 @@
+// var currentIndex = 0;  
+
 $('.tile-slider').each(function () {              // For every slider
     var $this = $(this);                    // Current slider
     var $group = $this.find('.horizontal-scroll-window-wrapper'); // Get the slide-group (container)
     var $slides = $this.find('.project-tile');       // Create jQuery object to hold all slides
     var $scollWindow = $this.find('.horizontal-scroll-window');       // Create jQuery object to hold all slides
+    var $slideNumber = $this.find('.slide-number-text');       // Create jQuery object to hold all slides
     var buttonArray = [];                    // Create array to hold navigation buttons
     var currentIndex = 0;                     // Hold index number of the current slide
     var timeout;                          // Sets gap between auto-sliding
-
-    $group.scrollLeft = 0;
+    window.addEventListener("load", function () {
+        $scollWindow.scrollLeft(0);
+        $slideNumber.text(1 + "/" + $slides.length);
+    });
+    
     $.each($slides, function (index) {
         // Create a button element for the button
         var $button = $('<div class="slide-btn"></div>');
@@ -17,23 +23,7 @@ $('.tile-slider').each(function () {              // For every slider
             }
         });
 
-        $button.on('click', function () { // Create event handler for the button
-            // move(index); 
-            // $slides.eq(index)[0].scrollIntoView({ behavior: "smooth" , block: "center", inline: "center"});
-            // $slides.eq(index)[0].focus();
-            // $slides.eq(index)[0].scroll({
-            //     top: 0,
-            //     left: 0,
-            //     behavior: 'smooth'
-            // });
-            // window.scroll()
-            // $button.addClass('active');
-            // $.each(buttonArray, function (j) {
-            //     if (j !== index) {
-            //         buttonArray[j].removeClass('active');
-            //     }
-            // });
-        }).appendTo('.tile-buttons');   // Add to the buttons holder
+        $button.appendTo('.tile-buttons');   // Add to the buttons holder
         buttonArray.push($button);       // Add it to the button array
     });
     let width = $group.width();
@@ -42,14 +32,13 @@ $('.tile-slider').each(function () {              // For every slider
     // advance();                          // Script is set up, advance() to move it
     $scollWindow.on("scroll", function (event) { 
         let tmp = currentIndex;
-        // currentIndex = 2;
-        // console.log($slides.length);
         console.log($scollWindow.scrollLeft());
         let tileWidth = Math.round(width / $slides.length);
         console.log(tileWidth);
         for (let i = 0; i < ($slides.length); i++) {
-            if ($scollWindow.scrollLeft() < (tileWidth + (tileWidth * i))) {
+            if ($scollWindow.scrollLeft() < (Math.round(tileWidth / 2) + (tileWidth * i))) {
                 currentIndex = i;
+                $slideNumber.text((i+1) + "/" + $slides.length);
                 break;
             }
         }
